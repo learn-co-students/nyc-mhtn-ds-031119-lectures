41276254bb72df536d60357b1fd652ed95839d13
Robert Hillery <robert.hillery@gmail.com>
2019-03-11 13:23:21 -0400

    add functions assessment

---
+++ functions.py
@@ -0,0 +1,29 @@
+# Make a function to determine if a number is odd or even
+
+def odd_even():
+    pass
+
+# Make a function that takes in a list of numbers and returns the numbers that are even
+
+def even_list(numbers):
+    pass
+
+# Given a list return the unique names in the list
+
+def unique_names(list_of_names):
+    pass
+
+# Make a function that determines if a word is a palindrome
+
+def palindrome_detector(string):
+    """
+    Input: string
+    returns : True/False"""
+    pass
+
+print(odd_even(4))
+print(odd_even(139))
+print(even_list([1,3,4,6,7]))
+print(unique_names(['john', 'john', 'john']))
+print(palindrome_detector('racecar'))
+print(palindrome_detector('not'))
