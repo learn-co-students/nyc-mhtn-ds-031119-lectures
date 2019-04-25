9ea155cfdecb023bf81998df929e6cf4da0519a7
Robert Hillery <robert.hillery@gmail.com>
2019-04-25 15:15:18 -0400

    rename and feature selection

---
+++ Mod_1/OOP/example_mod.py
@@ -0,0 +1,14 @@
+import random
+
+s = "Hey Class, how cool is this. I just brought in some stuff from a random python file"
+a = [100, 200, 300]
+
+
+def pick1(arg):
+    print(random.choice(arg))
+
+def greetings(name):
+    print("Hello my , "+name)
+
+class Foo:
+    pass
---
+++ Mod_1/Python-Fundamentals/functions.py
@@ -0,0 +1,37 @@
+# Make a function that takes a variable and checks to see if the number is even
+# odd_even(4) -> True
+# odd_even(3) -> False
+# odd_even('s') -> False
+
+def odd_even(variable):
+    if type(variable)
+    pass
+
+# Make a function that takes in a list of numbers and returns only the numbers that are even
+# even_list([1,2,3,4,5,6]) -> [2,4,6]
+
+def even_list(numbers):
+    pass
+
+# Given a list return the unique names in the list
+# for example unique_names(['SeanAbu', 'Robert', 'Yish', 'Robert', 'Yish'])
+# -> ['SeanAbu', 'Robert', 'Yish']
+
+def unique_names(list_of_names):
+    pass
+
+# Make a function that determines if a string is a palindrome and returns true if it is one or false otherwise
+# palindrome_detector('mamam') -> True
+# palindrome_detector('royal') _> False
+def palindrome_detector(string):
+    """
+    Input: string
+    returns : True/False"""
+    pass
+
+print(odd_even(4))
+print(odd_even(139))
+print(even_list([1,3,4,6,7]))
+print(unique_names(['john', 'john', 'john']))
+print(palindrome_detector('racecar'))
+print(palindrome_detector('not'))
